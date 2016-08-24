ea1026b5525b87ec5b7e92cb76acf144d903856e
inuscript <inuscript@gmail.com>
2016-08-25 00:30:53 +0900

    own

--- src/App.js
+++ src/App.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite'
 
-const style = StyleSheet.create({
-  background: {
-    opacity: 0.8,
+const baseStyles = {
+  wrapAll: {
+    height: "100%",
+    width: "100%",
     position: 'absolute',
     top: 0,
     left: 0,
-    height: "100%",
-    width: "100%",
-    background: "#000"
+  },
+  fadeIn(start=0, end=1) {
+    return {
+      animationName: {from: { opacity: start}, to: {opacity: end}},
+      animationDuration: "0.3s"
+    }
+  }
+}
+const style = StyleSheet.create({
+  container: {
+    ...baseStyles.wrapAll,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    ...baseStyles.wrapAll,
+    ...baseStyles.fadeIn(0, 0.8),
+    opacity: 0.8,
+    background: "#000",
   },
   closeButton: {
+    padding: 3,
+    textAlign: "right",
+    cursor: "pointer",
+    color: "gray",
   },
   dialog: {
+    ...baseStyles.fadeIn(0, 1),
+    zIndex: 100,
     boxSizing: 'border-box',
-    position: 'relative',
-    backgroundColor: "white"
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 4,
+    width: "auto",
+    transition: 'transform 0.1s',
   }
 })
 
-const Close = () => {
-  <div className={css(style.closeButton)}>×</div>
+const DialogHeader = ({onClose}) => {
+  return <div>
+    <div className={css(style.closeButton)} onClick={onClose}>× 閉じる</div>
+  </div>
 }
-const ModalBackground = () => {
-  return <div className={css(style.background)} />
+const ModalBackground = (props) => {
+  return <div {...props} className={css(style.background)} />
 }
-
 const Dialog = ({children}) => {
   return <div className={css(style.dialog)}>
     {children}
   </div>
 }
+
 const ItemA = () => {
   return <div>hoge</div>
 }
 
 const Modal = ({onClose, children}) => {
   return (
-    <div>
-      <ModalBackground />
+    <div className={css(style.container)} >
+      <ModalBackground onClick={onClose} />
       <Dialog>
+        <DialogHeader onClose={onClose} />
         {children}
       </Dialog>
     </div>
