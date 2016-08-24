e8547ce77e5277e9b14beb1c00b5a5ab0039f4c8
inuscript <inuscript@gmail.com>
2016-08-24 23:39:03 +0900

    self

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
-import {ModalContainer, ModalDialog, FlexDialog, ModalPortal} from 'react-modal-dialog';
+import { StyleSheet, css } from 'aphrodite'
 
+const style = StyleSheet.create({
+  background: {
+    opacity: 0.8,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    background: "#000"
+  },
+  closeButton: {
+  },
+  dialog: {
+    boxSizing: 'border-box',
+    position: 'relative',
+    backgroundColor: "white"
+  }
+})
+
+const Close = () => {
+  <div className={css(style.closeButton)}>×</div>
+}
+const ModalBackground = () => {
+  return <div className={css(style.background)} />
+}
+
+const Dialog = ({children}) => {
+  return <div className={css(style.dialog)}>
+    {children}
+  </div>
+}
 const ItemA = () => {
   return <div>hoge</div>
 }
 
-const Modal = ({onClose}) => {
+const Modal = ({onClose, children}) => {
   return (
-    <ModalContainer onClose={onClose}>
-      <ModalDialog onClose={onClose}>
-        <ItemA/>
-      </ModalDialog>
-    </ModalContainer>
+    <div>
+      <ModalBackground />
+      <Dialog>
+        {children}
+      </Dialog>
+    </div>
   );
 }
 
@@ -26,7 +58,11 @@ class App extends Component{
     if(!this.state.showModal){
       return <noscript />
     }
-    return <Modal onClose={this.handleClose}/>
+    return (
+      <Modal onClose={this.handleClose}>
+        <ItemA/>
+      </Modal>
+    )
   }
 }
 
