e1d04c8b6a9876ff7138c5571c53b9a21ccbd9d6
inuscript <inuscript@gmail.com>
2016-08-25 08:30:23 +0900

    fix mixin names

--- src/modal.style.js
+++ src/modal.style.js
@@ -1,37 +1,38 @@
 import { StyleSheet } from 'aphrodite'
 
-const mixin = {
-  wrapAll(){
-    return {
-      height: "100%",
-      width: "100%",
-      position: 'absolute',
-      top: 0,
-      left: 0,
-    }
+const fullScreen = () => ({
+  height: "100%",
+  width: "100%",
+  position: 'absolute',
+  top: 0,
+  left: 0,
+})
+
+const justifyParent = () => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+})
+
+const fadeIn = (start=0, end=1) => ({
+  animationName: {
+    from: { opacity: start },
+    to: { opacity: end }
   },
-  fadeIn(start=0, end=1) {
-    return {
-      animationName: {
-        from: { opacity: start }, to: { opacity: end }
-      },
-      animationDuration: "0.3s"
-    }
-  }
-}
+  animationDuration: "0.3s"
+})
+
 export default {
   container: {
-    ...mixin.wrapAll(),
-    ...mixin.fadeIn(0, 1),
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    ...fullScreen(),
+    ...justifyParent(),
+    ...fadeIn(0, 1),
   },
   containerHide: {
-    ...mixin.fadeIn(1, 0),
+    ...fadeIn(1, 0),
   },
   background: {
-    ...mixin.wrapAll(),
+    ...fullScreen(),
     opacity: 0.8,
     background: "#000",
   },
